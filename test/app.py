afbe36ac91a7379d0b0421c839b34240786485c5
Sophie Nichol <snichol00@stuy.edu>
2019-09-19 14:53:30 -0400

    added stuff

--- test/app.py
+++ test/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "hi"
+
+coll = [0,1,1,2,3,5,8]
 
 if __name__ == "__main__":
     app.debug = True
