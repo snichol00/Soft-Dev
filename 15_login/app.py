7e13090d7b12dd1d41ff043cc2238d4d48c38993
Sophie Nichol <snichol00@stuy.edu>
2019-10-02 22:14:45 -0400

    did everything but cookies and logout

--- 15_login/app.py
+++ 15_login/app.py
@@ -7,18 +7,53 @@ from flask import Flask, render_template, request
 import cgi
 app = Flask(__name__)
 
+myUsername = "happy"
+myPassword = "feet"
 
 @app.route("/")
 def loginPage():
     return render_template('login.html')
 
+@app.route("/auth")
+def authenticate():
+    print(app)
+    print(request)
+    print(request.headers)
+    #print(request.method)
+    print(request.args)
+    username = request.args['username']
+    password = request.args['password']
+    if (username == myUsername and password == myPassword):
+        return welcomePage()
+    else:
+        return errorPage()
+
 @app.route("/error")
 def errorPage():
-    return render_template('error.html')
+    username = request.args['username']
+    password = request.args['password']
+    if (username != myUsername and password != myPassword):
+        return render_template('error.html',
+                                username = request.args['username'],
+                                password = request.args['password'],
+                                errMessage = "Incorrect Username and Password. Try Again")
+    if (username != myUsername):
+        return render_template('error.html',
+                                username = request.args['username'],
+                                password = request.args['password'],
+                                errMessage = "Incorrect Username. Try Again")
+    else:
+        return render_template('error.html',
+                                username = request.args['username'],
+                                password = request.args['password'],
+                                errMessage = "Incorrect Username. Try Again")
+
 
 @app.route("/welcome")
 def welcomePage():
-    return render_template('welcome.html')
+    return render_template('welcome.html',
+                            username = request.args['username'],
+                            password = request.args['password'])
 
 if __name__ == "__main__":
     app.debug = True # Automatically updates project with save file
