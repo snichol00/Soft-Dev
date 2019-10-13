937d107a559f339f95e609d127bbb233272a0096
Sophie Nichol <snichol00@stuy.edu>
2019-10-13 17:08:46 -0400

    made initial table and calculated GPA

---
+++ 18_db-nmcrnch/stu_mean.py
@@ -0,0 +1,30 @@
+
+#Jun tao Lei, Grace Mao, Sophie Nichol
+#SoftDev1 pd9
+#K18: Average
+#2019-10-11
+
+import sqlite3
+import csv
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#create table for GPA's
+create_GPA_table = """ CREATE TABLE IF NOT EXISTS stu_avg (
+                                id INTEGER, avg INTEGER
+                            );"""
+c.execute(create_GPA_table)
+
+# finds the GPA of each student based on their ID
+def GPA(ID):
+    list_grades = str.format("SELECT mark FROM courses WHERE id = {}", ID) #accesses all grades from one student
+    all_grades = c.execute(list_grades)
+    grades = all_grades.fetchall()
+    sum = 0
+    for mark in grades:
+        sum += mark[0]
+    return sum/len(grades)
