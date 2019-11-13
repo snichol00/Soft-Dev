f44832a27951d8e2c3d149f3b1ac9db847c0ec65
Sophie Nichol <snichol00@stuy.edu>
2019-11-13 00:05:24 -0500

    fixed problem

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -3,21 +3,20 @@
 # K24 -- A RESTful Journey Skyward
 # 2019-11-12
 
-from flask import Flask, render_template, request
-import urllib3
+from flask import Flask, render_template
+import urllib.request as urllib2
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    url = "https://api.nasa.gov/planetary/apod?api_key=8T1e9uGa5Vv9I8qpGGBEeemUKpUXGtaqZnWMIecC"
-    http = urllib3.PoolManager()
-    response = http.request('GET', url)
-    data = json.loads(response.data)
+def root():
+    u = urllib2.urlopen("https://api.nasa.gov/planetary/apod?api_key=8T1e9uGa5Vv9I8qpGGBEeemUKpUXGtaqZnWMIecC")
+    response = u.read()
+    data = json.loads(response)
     return render_template("index.html",
-                            photo = data["url"],
-                            info = data["explanation"])
+                            photo=data['url'],
+                            info=data['explanation'])
 
 #main
 if __name__ == "__main__":
