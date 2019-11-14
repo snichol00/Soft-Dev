0d6f459d651fd066e9ccafc9275983d464d08db4
Sophie Nichol <snichol00@stuy.edu>
2019-11-13 22:33:33 -0500

    added all 3 APIs

--- 25_restrio_frenchFries/app.py
+++ 25_restrio_frenchFries/app.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
-import urllib.request as urllib2
+from flask import Flask, render_template, request
+from urllib.request import urlopen
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    u = urllib.urlrequest.urlopen("http://dnd5eapi.co/api/spells/1/")
+    # Dungeons and Dragons API
+    u = urlopen("http://dnd5eapi.co/api/spells/1/")
     response = u.read()
     data = json.loads(response)
+    info = data["desc"]
+    # Metaweather
+    u = urlopen("https://www.metaweather.com/api/location/44418/2013/4/27/")
+    response = u.read()
+    data = json.loads(response)
+    weather = data[0]["weather_state_name"]
+    # Lorem Ipsum
+    u = urlopen("http://taco-randomizer.herokuapp.com/random/?full-taco=true")
+    response = u.read()
+    data = json.loads(response)
+    rec = data["recipe"]
     return render_template("index.html",
-                            info = data["desc"])
+                            info = info,
+                            weather = weather,
+                            recipe = rec)
 
 
 #main
