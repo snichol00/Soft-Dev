343e7aee2c7b017fb405855abc41bc29d2ed3e92
Sophie Nichol <snichol00@stuy.edu>
2019-12-11 14:29:05 -0500

    updated gcd

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -10,15 +10,20 @@ var fibonacci = function(n){
   }
 }
 
-var gcd = function(x, y){
-  if (x > y){
-    var max = x;
-    var min = y;
-    return gcd (x % y, y);
-  }
-  else{
-    var max = y;
-    var min = x;
-    return gcd (y % x, x);
-  }
+var gcdH = function(a, b, c) {
+  if (a % c == 0 && b % c == 0) return c;
+  else return gcdH(a, b, c-1);
+};
+var gcd = function(a, b) {
+  if (a == b) return a;
+  else if (a < b) return gcdH(a, b, a);
+  else return gcdH(a, b, b);
+};
+
+var studentlist = ["Me", "You", "Ed", "Joe", "Shelley", "Martha", "Gerard", "Steve", "Sally"];
+
+var randomStudent = function(x){
+    var len = x.length;
+    var rand = Math.floor(Math.random(len) * len + 1);
+    return x[rand - 1];
 }
