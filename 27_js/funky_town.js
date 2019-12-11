ef71f169defdf34cd8f42d4ff3de9cbb11977175
Sophie Nichol <snichol00@stuy.edu>
2019-12-10 22:27:03 -0500

    finished hw

--- 27_js/funky_town.js
+++ 27_js/funky_town.js
@@ -14,11 +14,24 @@ var gcd = function(x, y){
   if (x > y){
     var max = x;
     var min = y;
-    return gcd (x % y, y);
   }
   else{
     var max = y;
     var min = x;
-    return gcd (y % x, x);
   }
+  var n = min;
+  while (n > 0){
+    if (min % n == 0 && max % n == 0){
+      return n;
+    }
+    n--;
+  }
+}
+
+var studentlist = ["Me", "You", "Ed", "Joe", "Shelley", "Martha", "Gerard", "Steve", "Sally"];
+
+var randomStudent = function(x){
+    var len = x.length;
+    var rand = Math.floor(Math.random(len) * len + 1);
+    return x[rand - 1];
 }
