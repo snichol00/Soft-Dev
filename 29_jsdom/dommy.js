67c967a900f520ce6571b31962183dcb148cf95d
Sophie Nichol <snichol00@stuy.edu>
2019-12-12 18:42:05 -0500

    typed up and got add item to work

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -1,26 +1,65 @@
-var fibButton = document.getElementById('fib');
-var fibClick = function(){
-  document.getElementById("fib");
-  result = fibonacci(7);
-  console.log(result);
-  //how to print to page?
+//changes heading when hovered
+var changeHeading = function(e){
+  var h = document.getElementById("h");
+  h.innerHTML = "???";
 };
-fibButton.addEventListener('click', fibClick());
 
-var gcdButton = document.getElementById('gcd');
-var gcdClick = function(){
-  document.getElementById("gcd");
-  result = gcd(81, 27);
-  console.log(result);
-  //how to print to page?
-};
-gcdButton.addEventListener('click', gcdClick());
+//Removes item from list when clicked
+var removeItem = function(e){
 
-var ranButton = document.getElementById('ran');
-var ranClick = function(){
-  document.getElementById("ran");
-  result = randomStudent(81, 27);
-  console.log(result);
-  //how to print to page?
 };
-ranButton.addEventListener('click', ranClick());
+
+//lists element and their events
+var lis = document.getElementsByTagName("li");
+
+console.log(lis);
+
+for(var i=0; i<lis.length; i++){
+  lis[i].addEventListener('mouseover', function(e){
+    changeHeading(e, lis[i]);
+  });
+  lis[i].addEventListener('mouseout', changeHeading)
+  lis[i].addEventListener('click', removeItem);
+};
+
+//adds item "WORD" to list
+var addItem = function(e){
+  var list = document.getElementById("thelist");
+  var item = document.createElement("li");
+  item.innerHTML = "WORD";
+  list.appendChild(item);
+};
+
+var button = document.getElementById("b");
+button.addEventListener('click', addItem)
+
+//Fib function
+var fib = function(n){
+  if(n < 2){
+    return 1;
+  }
+  else{
+    return fib(n-1) + fib(n-2);
+  }
+};
+
+//add a new fib to the list
+var addFib = function(e){
+  console.log(e);
+  //???
+}
+
+//should use dynamic programming (store recent vals)
+var addFib2 = function(e){
+  console.log(e);
+  //???
+}
+
+var fb = document.getElementById("fb");
+fb.addEventListener('click', addFib);
+
+
+//display event in console
+button.addEventListener('click', function(e){
+  console.log(e);
+});
