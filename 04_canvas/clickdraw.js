bd7a4fa3e09e05bb11f010fe81719a72c287ebc2
Sophie Nichol <snichol00@stuy.edu>
2020-02-05 22:12:10 -0500

    matched with partner

--- 04_canvas/clickdraw.js
+++ 04_canvas/clickdraw.js
@@ -1,6 +1,8 @@
 // true is rectangle
 var drawType = true;
 
+
+// ===================================================================================
 var toggle = function(e){
   // grabs the text that shows which mode we in
   var text = document.getElementById("toggletext");
@@ -18,26 +20,37 @@ var toggle = function(e){
 // assigns the button for toggling with the previous function
 var togglebutton = document.getElementById('toggle');
 togglebutton.addEventListener('click', toggle);
+// ===================================================================================
 
-var clear = function(e){
+// ===================================================================================
+var clearScreen = function(e){
+  // gets the canvas
+
   var c = document.getElementById("slate");
   var ctx = c.getContext("2d");
+  // adds white rectangle to clear screen
   ctx.clearRect(0,0,c.width,c.height);
 };
 
 var clearbutton = document.getElementById('clear');
-clearbutton.addEventListener('click', clear);
+clearbutton.addEventListener('click', clearScreen)
+// ===================================================================================
 
-var draw = function(e){
+// ===================================================================================
+var draw = function(event){
   var c = document.getElementById("slate");
   var ctx = c.getContext("2d");
-  if (drawType == true){
-    ctx.fillRect(e.pageX,e.pageY,5,10);
+  ctx.fillStyle = "#ff0000";
+  if(drawType == true){
+    ctx.fillRect(event.clientX - 2.5,event.clientY - 2.5,5,5);
   }
   else{
-    ctx.fillRect(e.pageX, e.pageY, 1, 1)
+    ctx.beginPath();
+    ctx.arc(event.clientX - 2.5, event.clientY - 2.5, 5, 0, 2 * Math.PI);
+    ctx.fill();
   }
 };
 
-var clickdraw = document.getElementById("slate");
-clickdraw.addEventListener('click', draw);
+var pen = document.getElementById("slate");
+pen.onmousedown = draw;
+// ===================================================================================
