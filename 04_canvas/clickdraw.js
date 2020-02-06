c607dcd5c973a7446d6bf547c5862e0357a6cfa4
Sophie Nichol <snichol00@stuy.edu>
2020-02-05 21:57:12 -0500

    added clear, need draw to test

---
+++ 04_canvas/clickdraw.js
@@ -0,0 +1,28 @@
+// true is rectangle
+var drawType = true;
+
+var toggle = function(e){
+  // grabs the text that shows which mode we in
+  var text = document.getElementById("toggletext");
+  // change the boolean to the opposite and change the text correspendingly
+  // false = dot | true = rectangle
+  if (drawType == true){
+    drawType = false;
+    text.innerHTML = "Dot";
+  }
+  else{
+    drawType = true;
+    text.innerHTML = "Rectangle";
+  }
+};
+// assigns the button for toggling with the previous function
+var togglebutton = document.getElementById('toggle');
+togglebutton.addEventListener('click', toggle);
+
+var clear = function(e){
+  var c = document.getElementById("slate");
+  var ctx = c.getContext("2d");
+  ctx.clearRect(0,0,c.width,c.height);
+
+var clearbutton = document.getElementById('clear');
+clearbutton.addEventListener('click', clear);
