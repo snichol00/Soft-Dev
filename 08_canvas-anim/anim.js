1643fa717c2412d3f44d4cb0ec436179840ac066
Sophie Nichol <snichol00@stuy.edu>
2020-02-14 03:07:58 -0500

    finsihed

--- 08_canvas-anim/anim.js
+++ 08_canvas-anim/anim.js
@@ -2,18 +2,24 @@ var canvas = document.getElementById("playground");
 var ctx = canvas.getContext("2d");
 
 var id = null;
-var radius = 1;
+var radius = 2;
 var direction = "out"; //out or in
+var circAnim = "off"; //on or off
+var dvdAnim = "off"; //on or off
+var x = 0;
+var y = 0;
+var dx = 2;
+var dy = 2;
 
 var logo = new Image();
-logo.src = "logo_dvd.jpg";
+logo.src = "logo_dvd.jpg"
 
-document.getElementById("go").addEventListener("click", () => {
-  window.requestAnimationFrame(circle);
-});
 
-document.getElementById("dvd").addEventListener("click", () => {
-  window.requestAnimationFrame(bounce);
+document.getElementById("circle").addEventListener("click", () => {
+  if(circAnim === "off"){
+    circle();
+    circAnim = "on";
+  }
 });
 
 function circle(){
@@ -25,10 +31,10 @@ function circle(){
     direction = "out";
   }
   if(direction === "out"){
-    radius++;
+    radius += 2;
   }
   else if(direction === "in"){
-    radius--;
+    radius -= 2;
   }
   //console.log(radius)
   ctx.fillStyle = "blue";
@@ -40,12 +46,37 @@ function circle(){
   }
 };
 
-function bounce(){
-  ctx.clearRect(0,0,600,600)
-  
+function randXY(){
+  x = Math.random() * (600 - logo.width/5);
+  y = Math.random() * (600 - logo.height/5);
+}
+
+document.getElementById("dvd").addEventListener("click", () => {
+  window.cancelAnimationFrame(id);
+  ctx.clearRect(0, 0, 600, 600);
+  circAnim = "off";
+  dvdAnim = "on"
+  id = null;
+  randXY();
+  moveDvd();
+});
+
+function moveDvd(){
+  ctx.drawImage(logo, x, y, logo.width/5, logo.height/5);
+  if(x + 2 > 600 - logo.width/5 || x + 2 < 0){
+    dx *= -1;
+  }
+  if(y + 2 > 600 - logo.height/5 || y + 2 < 0){
+    dy *= -1;
+  }
+  x += dx;
+  y += dy;
+  id = window.requestAnimationFrame(moveDvd);
 }
 
 document.getElementById("stop").addEventListener("click", () => {
   window.cancelAnimationFrame(id);
+  circAnim = "off";
+  dvdAnim = "off"
   id = null;
 });
