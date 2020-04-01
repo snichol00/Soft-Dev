23f3f2b6ebdf003ae4728d4de8f250ef80bba86a
Sophie Nichol <snichol00@stuy.edu>
2020-04-01 01:59:13 -0400

    update

--- 14_svg/svg.js
+++ 14_svg/svg.js
@@ -1,11 +1,17 @@
-//Emory Walsh & Sophie Nichol
+//Lauren Pehlivanian & Sophie Nichol
 //Softdev pd09
-//K12 -- Connect The Dots
+//K14 -- Ask Circles
 //2020-03-30
 
 var svg = document.getElementById("vimage");
-var motion = document.getElementById("move");
-var extra = document.getElementById("xtra");
+var move_but = document.getElementById("move");
+var xtra_but = document.getElementById("xtra");
+
+var moving = -1; //global to keep track if already moving
+
+//animation variables
+var window = document.defaultView;
+var id;
 
 var go = function(e){
   if (e.target == svg) {
@@ -39,41 +45,55 @@ var change = function(e){
   }
 }
 
-
-var move = function(e) {
-  var allC = document.getElementsByTagName("circle");
-  for (var i = 0; i < allC.length; i++) {
-    var xCor =  parseInt(allC[i].getAttribute("cx")) + parseInt(allC[i].getAttribute("xInc"));
-    var yCor =  parseInt(allC[i].getAttribute("cy")) + parseInt(allC[i].getAttribute("yInc"));
-    //if out of bounds change direction
-    if (yCor <= 5 || yCor >= 495){
-      allC[i].setAttribute("yInc", parseInt(allC[i].getAttribute("yInc") * -1));
+var move = function(timestamp) {
+  if (moving == 0) {
+    var allC = document.getElementsByTagName("circle");
+    for (var i = 0; i < allC.length; i++) {
+      //console.log("mv")
+      var xCor =  parseInt(allC[i].getAttribute("cx")) + parseInt(allC[i].getAttribute("xInc"));
+      var yCor =  parseInt(allC[i].getAttribute("cy")) + parseInt(allC[i].getAttribute("yInc"));
+      //if out of bounds change direction
+      if (yCor <= 5 || yCor >= 495){
+        allC[i].setAttribute("yInc", parseInt(allC[i].getAttribute("yInc") * -1));
+      }
+      if (xCor <= 5 || xCor >= 495){
+        allC[i].setAttribute("xInc", parseInt(allC[i].getAttribute("xInc") * -1));
+      }
+      allC[i].setAttribute("cy", yCor);
+      allC[i].setAttribute("cx", xCor);
     }
-    if (xCor <= 5 || xCor >= 495){
-      allC[i].setAttribute("xInc", parseInt(allC[i].getAttribute("xInc") * -1));
-    }
-    allC[i].setAttribute("cy", yCor);
-    allC[i].setAttribute("cx", xCor);
+    id = window.requestAnimationFrame(move);
   }
-  window.requestAnimationFrame(move);
-});
+};
 
+var xtra = function() {
+  var allC = document.getElementsByTagName("circle");
+  for (var i = 0; i<allC.length; i++) {
+    console.log("hi")
+    allC[i].setAttribute("r", Math.floor(Math.random() * 20) + 3);
+  }
+}
+
+//clear each child
 document.getElementById("clear").addEventListener("click", () => {
   while (svg.lastChild) {
     svg.removeChild(svg.lastChild);
   }
 });
 
-//change size of circles to random
-var Xtra = function(e) {
-  var allC = document.getElementsByTagName("circle");
-  var num = allC.length;
-  for (var i = 0; i < num; i++) {
-    allC[i].setAttribute("cx", Math.floor(Math.random() * 500));
-    allC[i].setAttribute("cy", Math.floor(Math.random() * 500));
-  }
-}
-
-extra.addEventListener('click', Xtra);
+//event listeners
 svg.addEventListener('click', go);
-motion.addEventListener('click', move);
+
+move_but.addEventListener('click', function() {
+  //only begin animation if not already moving
+  if (moving == -1) {
+    id = window.requestAnimationFrame(move);
+    moving = 0;
+    move();
+    //if already moving stop
+  } else {
+    moving = -1;
+  }
+});
+
+xtra_but.addEventListener('click', xtra);
