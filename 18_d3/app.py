7b365fb899a278945201769a1d2e7ab5a8bec00e
Sophie Nichol <snichol00@stuy.edu>
2020-04-16 14:21:11 -0400

    update

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -6,15 +6,21 @@ app = Flask(__name__)
 def root():
     return render_template("index.html")
 
-@app.route("/data", methods={'GET'})
+@app.route("/data", methods={"GET"})
 def data():
-    schools = []
-    with open('data.csv') as scoreFile:
-        scorereader = csv.DictReader(scoreFile)
-        for school in scorereader:
-            schools.append({'name': school[1], 'reading': school[3], 'math': school[4], 'writing': school[5]})
+    data = []
+    with open("data.csv") as csv_file:
+        reader = csv.reader(csv_file)
+        # skip the first row, which is the header row
+        next(reader)
+        for entry in reader:
+            data.append({'school': entry[1], 'testtakers': entry[2], 'reading': entry[3], 'math': int(entry[4]), 'writing': entry[5]})
 
-    return jsonify(schools)
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
 
 
 if __name__ == "__main__":
